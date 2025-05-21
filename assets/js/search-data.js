9aaf5648b2bb5c4c1cbbda91eedc71c89b331586
yunbinchosts <yunbincho@g.ecc.u-tokyo.ac.jp>
2025-05-21 08:18:14 +0000

    Deploying to gh-pages from @ yunbinchosts/yunbinchosts.github.io@0c5c929db7759ba9cdb213b88840c8048e2ab8b1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -345,17 +345,6 @@ ninja.data = [{
             window.location.href = "/blog/2020/twitter/";
           
         },
-      },{id: "post-a-post-with-disqus-comments",
-        
-          title: "a post with disqus comments",
-        
-        description: "an example of a blog post with disqus comments",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/disqus-comments/";
-          
-        },
       },{id: "post-a-post-with-math",
         
           title: "a post with math",
@@ -367,17 +356,6 @@ ninja.data = [{
             window.location.href = "/blog/2015/math/";
           
         },
-      },{id: "post-a-post-with-code",
-        
-          title: "a post with code",
-        
-        description: "an example of a blog post with some code",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/code/";
-          
-        },
       },{id: "post-a-post-with-images",
         
           title: "a post with images",
@@ -387,17 +365,6 @@ ninja.data = [{
         handler: () => {
           
             window.location.href = "/blog/2015/images/";
-          
-        },
-      },{id: "post-a-post-with-formatting-and-links",
-        
-          title: "a post with formatting and links",
-        
-        description: "march &amp; april, looking forward to summer",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/formatting-and-links/";
           
         },
       },{id: "news-a-simple-inline-announcement",
