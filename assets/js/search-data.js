fa52c3e38884723ea4662ba1d20a202d5784cbde
yunbinchosts <yunbincho@g.ecc.u-tokyo.ac.jp>
2025-05-21 08:47:41 +0000

    Deploying to gh-pages from @ yunbinchosts/yunbinchosts.github.io@49d5cafe2ef46139c3cb745f9672333ce851f748 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -224,6 +224,17 @@ ninja.data = [{
             window.location.href = "/blog/2023/videos/";
           
         },
+      },{id: "post-displaying-beautiful-tables-with-bootstrap-tables",
+        
+          title: "displaying beautiful tables with Bootstrap Tables",
+        
+        description: "an example of how to use Bootstrap Tables",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2023/tables/";
+          
+        },
       },{id: "post-displaying-external-posts-on-your-al-folio-blog",
         
           title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
