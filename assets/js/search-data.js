379297ca60c7530251dfb8eef4b190975afdf572
yunbinchosts <yunbincho@g.ecc.u-tokyo.ac.jp>
2025-05-21 08:46:57 +0000

    Deploying to gh-pages from @ yunbinchosts/yunbinchosts.github.io@cbb1db2dea2b2203e31205a0aa4ae594fd84a337 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -59,17 +59,6 @@ ninja.data = [{
             window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
           
         },
-      },{id: "post-a-post-with-tabs",
-        
-          title: "a post with tabs",
-        
-        description: "this is what included tabs in a post could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/tabs/";
-          
-        },
       },{id: "post-a-post-with-typograms",
         
           title: "a post with typograms",
@@ -233,28 +222,6 @@ ninja.data = [{
         handler: () => {
           
             window.location.href = "/blog/2023/videos/";
-          
-        },
-      },{id: "post-displaying-beautiful-tables-with-bootstrap-tables",
-        
-          title: "displaying beautiful tables with Bootstrap Tables",
-        
-        description: "an example of how to use Bootstrap Tables",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/tables/";
-          
-        },
-      },{id: "post-a-post-with-table-of-contents",
-        
-          title: "a post with table of contents",
-        
-        description: "an example of a blog post with table of contents",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/table-of-contents/";
           
         },
       },{id: "post-displaying-external-posts-on-your-al-folio-blog",
